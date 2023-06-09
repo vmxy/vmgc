d566e0996c73fe565df98a6942d8fcee135c3a53
sd <leowest1921@gmail.com>
2023-06-09 12:17:43 +0800

    add

--- src/router/guard/index.ts
+++ src/router/guard/index.ts
@@ -2,7 +2,7 @@ import type { Router } from "vue-router";
 import { useTitle } from "@vueuse/core";
 import { t } from "@/locales";
 import { createPermissionGuard } from "./permission";
-
+const ssr = import.meta.env.SSR;
 /**
  * 路由守卫函数
  * @param router - 路由实例
@@ -10,7 +10,7 @@ import { createPermissionGuard } from "./permission";
 export function createRouterGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
     // 开始 loadingBar
-    window.$loadingBar?.start();
+    ssr || globalThis.$loadingBar?.start();
     // 页面跳转权限处理
     await createPermissionGuard(to, from, next);
   });
@@ -18,6 +18,6 @@ export function createRouterGuard(router: Router) {
     // 设置document title
     useTitle(to.meta.i18nTitle ? t(to.meta.i18nTitle) : to.meta.title);
     // 结束 loadingBar
-    window.$loadingBar?.finish();
+    ssr || globalThis.$loadingBar?.finish();
   });
 }
