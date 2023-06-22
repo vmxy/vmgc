38b76b309d3ed566e6e7b63fc2f64bbfb367d35b
sd <leowest1921@gmail.com>
2023-06-22 14:22:59 +0800

    add

---
+++ src/layouts/common/global-header/components/global-goback.vue
@@ -0,0 +1,21 @@
+<template>
+  <hover-container class="w-40px h-full" tooltip-content="后退" placement="bottom-end" @click="handleGo">
+    <icon-mdi-arrow-left class="text-22px" :class="{ 'animate-spin': loading }" />
+  </hover-container>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { useAppStore, useRouteStore } from "@/store";
+import { useLoading } from "@/hooks";
+defineOptions({ name: "ReloadButton" });
+
+const { loading, startLoading, endLoading } = useLoading();
+
+function handleGo() {
+  if(ssr) return;
+  globalThis.history.go(-1);
+}
+</script>
+
+<style scoped></style>
