fee0c8c0e9b4a940e44ac3c9996ab8ed4bf9de03
sd <leowest1921@gmail.com>
2023-06-24 19:12:21 +0800

    add

---
+++ build/git.commit.ts
@@ -0,0 +1,7 @@
+import { version } from "../package.json";
+import pkg from "./electron/package.json";
+import fs from "fs";
+pkg.version = version;
+console.info("git pre-commmit set version to electron");
+
+fs.writeFileSync("./electron/package.json", JSON.stringify(pkg));
