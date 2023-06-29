a49b154ea0b0c0025d29525638242939c230578d
sd <leowest1921@gmail.com>
2023-06-29 17:07:43 +0800

    add

--- src/directives/lazyimg.ts
+++ src/directives/lazyimg.ts
@@ -8,12 +8,14 @@ import { useIntersectionObserver } from "@vueuse/core";
 export default function setupLazyimgDirective(app: App) {
   // app 实例身上有我们想要的全局注册指令方法  调用即可
   app.directive("lazyimg", {
-    mounted(el, binding) {
+    mounted: async (el, binding) => {
       if (ssr) return;
+      await wait(10);
       // el:img dom对象
       // binding.value  图片url地址
       // 使用 vueuse/core 提供的监听 api 对图片 dom 进行监听 正式进入视口才加载
       // img.src = url
+
       const { stop } = useIntersectionObserver(
         // 监听目标元素
         el,
