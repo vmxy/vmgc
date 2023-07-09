77ce997e41ada8ad821a8d120727b0137cdb97ef
sd <leowest1921@gmail.com>
2023-07-09 11:38:23 +0800

    add

---
+++ public/js/head.js
@@ -0,0 +1,16 @@
+function trycatch(handle) {
+  try {
+    if (!handle) return;
+    if (/function AsyncFunction/i.test(handle.constructor.toString())) {
+      handle().catch((err) => console.error("trycatch error", err));
+    } else {
+      handle();
+    }
+  } catch (err) {
+    console.error("trycatch error2", err);
+  }
+}
+
+trycatch(async() => {
+
+});
