9ddf46a594a696f9e1573199acee0025cab26c5f
sd <leowest1921@gmail.com>
2023-07-10 23:12:36 +0800

    add

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 (async () => {
   let { app, router } = createApp({ Page: App });
   //appLoading.mount("#appLoading");
+  
   router.isReady().then(() => {
     globalThis.document?.querySelector("#appLoading")?.remove();
     app.mount("#app");
